a2e5e7d44415a374707cd9ce469a98e778644a4e
Nick Van de Velden <nickvandevelden@hotmail.comgit config --global user.email nickvandevelden@hotmail.com>
2020-10-12 22:55:01 +0200

    added environment code and removed rectangle code

--- src/content.js
+++ src/content.js
@@ -12,6 +12,14 @@ chrome.extension.onMessage.addListener(function (request) {
 
     let environment;
 
+    if (url.includes('://testaboshop')) {
+      environment = 'test';
+    } else if (url.includes('://previewaboshop')) {
+      environment = 'preview';
+    } else if (url.includes('://aboshop')) {
+      environment = '';
+    }
+
     let msg = {
       currentUrl: url,
       currentOfferFomulaId: getOfferFormulaId(),
@@ -21,26 +29,3 @@ chrome.extension.onMessage.addListener(function (request) {
     chrome.runtime.sendMessage(msg);
   }
 });
-
-function drawRectangle(color) {
-  const x = 0;
-  const y = 0;
-  const width = 3840;
-  const height = 10;
-
-  const rectangle = document.createElement('canvas');
-  rectangle.style.width = '100%';
-  rectangle.style.height = '100%';
-  rectangle.width = window.innerWidth;
-  rectangle.height = window.innerHeight;
-  rectangle.style.position = 'fixed';
-  rectangle.style.left = 0;
-  rectangle.style.top = 0;
-  rectangle.style.zIndex = 100000;
-  rectangle.style.pointerEvents = 'none';
-  document.body.appendChild(rectangle);
-  const context = rectangle.getContext('2d');
-  context.rect(x, y, width, height);
-  context.fillStyle = color;
-  context.fill();
-}
